40963810c2463bb6259a559f93bea102cd4e5177
Aman <amandeep1994@msn.com>
2022-05-13 19:31:24 +0100

    Milestone1 deliverables

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Header.css" 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Header() {
+  return (
+    <div className='header'>
+    <p class='ml-4' >AR Salon & Day Spa</p>
+    </div>
+
+  )
+}
+
+export default Header
